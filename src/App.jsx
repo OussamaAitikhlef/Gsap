3e464a54e474a875984f4a1bb81ff0e1def80f07
Oussama Ait-ikhlef <oussamaaitikhlf@gmail.com>
2025-09-26 18:48:14 +0100

    feat: add Story and custom Cursor components
    Changes:
    - Add 'Our Story' section to main layout
    - Implement custom cursor component for visual enhancement
    
    Notes:
    Improves user experience and content richness by enhancing interactivity and page content.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import NavBar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import OurStory from "./components/OurStory";
+import TargetCursor from "./components/TargetCursor";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
     <main>
+      <TargetCursor />
       <NavBar />
       <Hero />
       <Cocktails />
+      <OurStory />
     </main>
   );
 }
