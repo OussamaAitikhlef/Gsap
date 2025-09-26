6a8b8661f895229fa86c922f1b8b903085052c86
Oussama Ait-ikhlef <oussamaaitikhlf@gmail.com>
2025-09-26 15:46:03 +0100

    feat: add Cocktails section with GSAP parallax
    Changes:
    - Introduce new Cocktails component rendering popular cocktails & mocktails
    - Implement GSAP ScrollTrigger parallax effect for decorative leaves
    - Import and render <Cocktails /> in App.jsx to include section in main layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import NavBar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    // max-w-6xl mx-auto flex items-center justify-between py-4 px-6
-    <main >
+    <main>
       <NavBar />
       <Hero />
+      <Cocktails />
     </main>
   );
 }
